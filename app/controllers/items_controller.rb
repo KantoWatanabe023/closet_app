6fe30d534f233066b02527c648ec98d0706fe783
KantoWatanabe023 <dash9air6k@icloud.com>
2021-06-18 23:00:31 +0900

    conflict解消

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,4 +1,9 @@
 class ItemsController < ApplicationController
+
+  before_action :set_item, only: [:show, :edit, :update, :destroy]
+
+
+
   def index
     @item = Item.all.order("created_at DESC")
   end
@@ -16,9 +21,34 @@ class ItemsController < ApplicationController
     end
   end
 
+
+  def show
+  end
+
+  def edit
+  end
+
+  def update
+    if @item.update(item_params)
+      redirect_to item_path
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @item.destroy
+    redirect_to root_path
+  end
+
+
   private
 
   def item_params
     params.require(:item).permit(:item_type_id,:season_id,:buy_day,:color_id,:size_id,:price,:memo, :image)
+  end
+
+  def set_item
+    @item = Item.find(params[:id])
   end
 end
